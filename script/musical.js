1f2706f61070c67aff48ece09c55b0805ece2924
ChiliPaneer <venkatratnamcp@gmail.com>
2021-03-31 01:20:44 -0400

    finishing touches complete

--- script/musical.js
+++ script/musical.js
@@ -3,90 +3,91 @@
 //import musicDataFile from "./data.js";
 
 const mvData = {
-    data: [
-        {
-            Title: "Nature the Magnificent - Musical performed in Jacksonville, FL Subhakaram",
-            Description: "This is the video of “Nature - the magnificent”, a musical which turned into a magnum opus surpassing all expectations of creators with all the performers excelling to their capacity. With dances in five forms, over 40 dancers and 20 musicians from three different cities in two states, and about 40 support crew, this musical was staged in Jacksonville, FL on Feb’03, 2018.        Song lyrics were by Dr.Uma Eyyunni, Concept, Script, Music and Direction was by Srinivasa Bharadwaj Kishore.        Intended from the beginning to be a fundraiser for relief of hurricane victims of Puerto Rico, donations are routed to the beneficiaries via Habitat for Humanity, Puerto Rico.",
-            Date: "February 03, 2018",
-            Location: "Jacksonville, Florida",
-            ytlink: "https://www.youtube.com/watch?v=mqseUISxKS8",
-            videoID: "mqseUISxKS8",
-
-        },
-        {
-
-            Title: "Nature the Magnificent - Musical performed in Jacksonville, FL Subhakaram",
-            Description: "This is the video of “Nature - the magnificent”, a musical which turned into a magnum opus surpassing all expectations of creators with all the performers excelling to their capacity. With dances in five forms, over 40 dancers and 20 musicians from three different cities in two states, and about 40 support crew, this musical was staged in Jacksonville, FL on Feb’03, 2018.        Song lyrics were by Dr.Uma Eyyunni, Concept, Script, Music and Direction was by Srinivasa Bharadwaj Kishore.        Intended from the beginning to be a fundraiser for relief of hurricane victims of Puerto Rico, donations are routed to the beneficiaries via Habitat for Humanity, Puerto Rico.",
-            Date: "February 03, 2018",
-            Location: "Jacksonville, Florida",
-            ytlink: "https://www.youtube.com/watch?v=mqseUISxKS8",
-            videoID: "mqseUISxKS8",
-        },
-        {
-
-            Title: "Nature the Magnificent - Musical performed in Jacksonville, FL Subhakaram",
-            Description: "This is the video of “Nature - the magnificent”, a musical which turned into a magnum opus surpassing all expectations of creators with all the performers excelling to their capacity. With dances in five forms, over 40 dancers and 20 musicians from three different cities in two states, and about 40 support crew, this musical was staged in Jacksonville, FL on Feb’03, 2018.        Song lyrics were by Dr.Uma Eyyunni, Concept, Script, Music and Direction was by Srinivasa Bharadwaj Kishore.        Intended from the beginning to be a fundraiser for relief of hurricane victims of Puerto Rico, donations are routed to the beneficiaries via Habitat for Humanity, Puerto Rico.",
-            Date: "February 03, 2018",
-            Location: "Jacksonville, Florida",
-            ytlink: "https://www.youtube.com/watch?v=mqseUISxKS8",
-            videoID: "mqseUISxKS8",
-        },
-    ],
-}
-
-
+  data: [
+    {
+      Title: "Nature the Magnificent - Musical performed in Jacksonville, FL",
+      Description:
+        "“Nature - the magnificent”, is a musical which turned into a magnum opus surpassing all expectations of the creators with all the performers excelling in their role.  This musical was conceived and produced as a fundraiser for the relief of hurricane victims of Puerto Rico, with the donations routed to the beneficiaries via Habitat for Humanity, Puerto Rico.\n\nThe production involved dances in five different forms, with over 40 dancers and 20 musicians from three different cities in two states in the US.  With support from about 40 crew members, this musical was staged in Jacksonville, FL on 3rd February, 2018.\n\nLyrics for the songs were written by Dr. Uma Eyyunni.\nConcept, Script, Music and Direction was by Srinivasa Bharadwaj Kishore.",
+      Date: "3-Feb-18",
+      Location: "Jacksonville, Florida",
+      ytlink: "https://www.youtube.com/watch?v=mqseUISxKS8",
+      videoID: "mqseUISxKS8",
+    },
+    {
+      Title: "Nature the Magnificent - Musical performed in Tallahassee, FL",
+      Description:
+        "“Nature - the magnificent”, is a musical which turned into a magnum opus surpassing all expectations of the creators with all the performers excelling in their role.  This musical was conceived and produced as a fundraiser for the relief of hurricane victims of Puerto Rico, with the donations routed to the beneficiaries via Habitat for Humanity, Puerto Rico.\n\nThe production involved dances in five different forms, with over 40 dancers and 20 musicians from three different cities in two states in the US.  With support from about 40 crew members, this musical was staged in Tallahassee, FL on 4th February, 2018.\n\nLyrics for the songs were written by Dr. Uma Eyyunni.\nConcept, Script, Music and Direction was by Srinivasa Bharadwaj Kishore.",
+      Date: "4-Feb-18",
+      Location: "Jacksonville, Florida",
+      ytlink: "https://www.youtube.com/watch?v=nJZyOOFD0f0&t=493s",
+      videoID: "nJZyOOFD0f0",
+    },
+    {
+      Title: "Neeharika Musical",
+      Description:
+        'Neeharika" a dance ballet based on the five elements - Earth, Sun, Moon, Wind and Water was produced and presented as a dance ballet in Tallahassee and Tampa, Florida in 2008.  This musical was produced as a fundraiser for the relief of hurricane Katrina victims.  This video is a part from Scene-1 of the dance ballet "Neeharika" presented at Tallahassee. \n\nLyrics for the musical are written by Dr Uma Eyyunni.   \nMusic and orchestration was by Srinivasa Bharadwaj Kishore.',
+      Date: 2008,
+      Location: "Tallahassee, Florida",
+      ytlink: "https://www.youtube.com/watch?v=T3Ws6Yzj9LI",
+      videoID: "T3Ws6Yzj9LI",
+    },
+  ],
+};
 
 function makeMV(mvData) {
-    let mvShell = document.createElement("div");
-    mvShell.setAttribute("class", "mv-shell");
+  let mvShell = document.createElement("div");
+  mvShell.setAttribute("class", "mv-shell");
 
-    let mvText = document.createElement("div");
-    mvText.setAttribute("class", "mv-text");
+  let mvText = document.createElement("div");
+  mvText.setAttribute("class", "mv-text");
 
-    let mvTitle = document.createElement("div");
-    mvTitle.setAttribute("class", "mv-title");
-    mvTitle.textContent = mvData.Title
+  let mvTitle = document.createElement("div");
+  mvTitle.setAttribute("class", "mv-title");
+  mvTitle.textContent = mvData.Title;
 
-    let mvDate = document.createElement("div");
-    mvDate.setAttribute("class", "mv-date");
-    mvDate.textContent = mvData.Date
+  let mvDate = document.createElement("div");
+  mvDate.setAttribute("class", "mv-date");
+  mvDate.textContent = mvData.Date;
 
-    let mvDesc = document.createElement("div");
-    mvDesc.setAttribute("class", "mv-desc");
-    mvDesc.textContent = mvData.Description
+  let mvDesc = document.createElement("div");
+  mvDesc.setAttribute("class", "mv-desc");
+  mvDesc.textContent = mvData.Description;
 
-    let mvContainer = document.createElement("div");
-    mvContainer.setAttribute("class", "mv-container");
+  let mvContainer = document.createElement("div");
+  mvContainer.setAttribute("class", "mv-container");
 
-    let mv = document.createElement("iframe");
-    mv.setAttribute("width", "100%");
-    mv.setAttribute("height", "240px");
-    mv.setAttribute("style", "border-radius: 20px; padding: 10px");
-    mv.setAttribute("src", "https://www.youtube-nocookie.com/embed/" + mvData.videoID);
-    mv.setAttribute("frameborder", "0");
-    mv.setAttribute("allow", "allow", "fullscreen; accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture");
+  let mv = document.createElement("iframe");
+  mv.setAttribute("width", "100%");
+  mv.setAttribute("height", "240px");
+  mv.setAttribute("style", "border-radius: 20px; padding: 10px");
+  mv.setAttribute(
+    "src",
+    "https://www.youtube-nocookie.com/embed/" + mvData.videoID
+  );
+  mv.setAttribute("frameborder", "0");
+  mv.setAttribute(
+    "allow",
+    "allow",
+    "fullscreen; accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+  );
 
-    mvContainer.appendChild(mv);
+  mvContainer.appendChild(mv);
 
-    mvText.appendChild(mvTitle);
-    mvText.appendChild(mvDate);
-    mvText.appendChild(mvDesc);
+  mvText.appendChild(mvTitle);
+  mvText.appendChild(mvDate);
+  mvText.appendChild(mvDesc);
 
-    mvShell.appendChild(mvText);
-    mvShell.appendChild(mvContainer);
+  mvShell.appendChild(mvText);
+  mvShell.appendChild(mvContainer);
 
-    return mvShell;
-
-
+  return mvShell;
 }
 
-
 function populateMV() {
-    let main = document.getElementById("mv-box");
-    mvData.data.forEach((mv) => {
-        main.appendChild(makeMV(mv));
-    });
+  let main = document.getElementById("mv-box");
+  mvData.data.forEach((mv) => {
+    main.appendChild(makeMV(mv));
+  });
 }
 
 populateMV();
-
--- script/mv.js
+++ script/mv.js
@@ -3,90 +3,128 @@
 //import musicDataFile from "./data.js";
 
 const mvData = {
-    data: [
-        {
-            Title: "Nature the Magnificent - Musical performed in Jacksonville, FL Subhakaram",
-            Description: "This is the video of “Nature - the magnificent”, a musical which turned into a magnum opus surpassing all expectations of creators with all the performers excelling to their capacity. With dances in five forms, over 40 dancers and 20 musicians from three different cities in two states, and about 40 support crew, this musical was staged in Jacksonville, FL on Feb’03, 2018.        Song lyrics were by Dr.Uma Eyyunni, Concept, Script, Music and Direction was by Srinivasa Bharadwaj Kishore.        Intended from the beginning to be a fundraiser for relief of hurricane victims of Puerto Rico, donations are routed to the beneficiaries via Habitat for Humanity, Puerto Rico.",
-            Date: "February 03, 2018",
-            Location: "Jacksonville, Florida",
-            ytlink: "https://www.youtube.com/watch?v=mqseUISxKS8",
-            videoID: "mqseUISxKS8",
-
-        },
-        {
-
-            Title: "Nature the Magnificent - Musical performed in Jacksonville, FL Subhakaram",
-            Description: "This is the video of “Nature - the magnificent”, a musical which turned into a magnum opus surpassing all expectations of creators with all the performers excelling to their capacity. With dances in five forms, over 40 dancers and 20 musicians from three different cities in two states, and about 40 support crew, this musical was staged in Jacksonville, FL on Feb’03, 2018.        Song lyrics were by Dr.Uma Eyyunni, Concept, Script, Music and Direction was by Srinivasa Bharadwaj Kishore.        Intended from the beginning to be a fundraiser for relief of hurricane victims of Puerto Rico, donations are routed to the beneficiaries via Habitat for Humanity, Puerto Rico.",
-            Date: "February 03, 2018",
-            Location: "Jacksonville, Florida",
-            ytlink: "https://www.youtube.com/watch?v=mqseUISxKS8",
-            videoID: "mqseUISxKS8",
-        },
-        {
-
-            Title: "Nature the Magnificent - Musical performed in Jacksonville, FL Subhakaram",
-            Description: "This is the video of “Nature - the magnificent”, a musical which turned into a magnum opus surpassing all expectations of creators with all the performers excelling to their capacity. With dances in five forms, over 40 dancers and 20 musicians from three different cities in two states, and about 40 support crew, this musical was staged in Jacksonville, FL on Feb’03, 2018.        Song lyrics were by Dr.Uma Eyyunni, Concept, Script, Music and Direction was by Srinivasa Bharadwaj Kishore.        Intended from the beginning to be a fundraiser for relief of hurricane victims of Puerto Rico, donations are routed to the beneficiaries via Habitat for Humanity, Puerto Rico.",
-            Date: "February 03, 2018",
-            Location: "Jacksonville, Florida",
-            ytlink: "https://www.youtube.com/watch?v=mqseUISxKS8",
-            videoID: "mqseUISxKS8",
-        },
-    ],
-}
-
-
+  data: [
+    {
+      Title: "We Belong - Song",
+      Description:
+        "This beautiful song describing the mindset of mentally ill written by Dr.Uma Eyyunni was set to music by Srinivasa Bharadwaj Kishore (kiBaSri).  The song was performed by the Jacksonville and Tallahassee singers during the presentation of the musical - Nature the Magnificent, in Tallahassee, FL.",
+      Date: "February 04, 2018",
+      Location: "Tallahassee, FL",
+      ytlink: "https://www.youtube.com/watch?v=SN0ooNAz6BI",
+      videoID: "SN0ooNAz6BI",
+    },
+    {
+      Title: "We Belong Song for Mother's Cry Inc",
+      Description:
+        "This beautiful song describing the mindset of mentally ill written by Dr.Uma Eyyunni was set to music by Srinivasa Bharadwaj Kishore (kiBaSri). This song was sung by Sravanti PanchAgnula and kiBaSrI.",
+      Date: 2017,
+      Location: "Jacksonville, Florida",
+      ytlink: "https://www.youtube.com/watch?v=AphbfRanXt0",
+      videoID: "AphbfRanXt0",
+    },
+    {
+      Title: "We Belong Song for NAMI by Mother's Cry Inc",
+      Description:
+        "This song was recreated for National Alliance on Mental Illness, with lyrics written by Dr Uma Eyyuni, illustrations by the cartoonist Jayadev Babu and the Music, Orchestration and Presentation by Srinivasa Bharadwaj Kishore",
+      Date: 2016,
+      Location: "Jacksonville, Florida",
+      ytlink: "https://www.youtube.com/watch?v=YmAzcVAPV0Q",
+      videoID: "YmAzcVAPV0Q",
+    },
+    {
+      Title: "sphaTikaliMgAkRtiM Song",
+      Description:
+        "This very beautiful song depicting Siva's tandavam written by Dr Uma Eyyunni was set to music and orchestrated by Srinivasa Bharadwaj Kishore. This version was sung by Srinivasa Bharadwaj Kishore and Sravanti Panchagnula.",
+      Date: 2012,
+      Location: "",
+      ytlink: "https://www.youtube.com/watch?v=4olSxclr2Jo",
+      videoID: "4olSxclr2Jo",
+    },
+    {
+      Title: "AShADhaM_vaccEnadigO Song",
+      Description:
+        "This beautiful telugu folk song was performed at ATA-2012 convention in Atlanta. Lyrics are by Dr Uma Eyyunni and music and orchestration was by Srinivasa Bharadwaj Kishore",
+      Date: "July 7, 2012",
+      Location: "Atlanta, GA",
+      ytlink: "https://www.youtube.com/watch?v=U5VO30itdHE",
+      videoID: "U5VO30itdHE",
+    },
+    {
+      Title:
+        'punnami rElu - "kOlATam" song and vaccEyaMDi - comedy musical skit following the song',
+      Description:
+        'This beautiful kOlATam song written by Dr Uma Eyyunni was set to music and was orchestrated by Srinivasa Bharadwaj Kishore.    It was sung by Radhika Nori.\n\nThe musical skit "vaccEyaMDi" follows the song.   Participants are Drs Uma and Ram Eyyunni, Srinivasa and Surekha Kishore, Seema and Seema Kuravi.  Writing the script jointly along with Dr Uma, Srinivasa Bharadwaj Kishore has set this skit to music.',
+      Date: 2006,
+      Location: "Ugadi celebrations by TAJA, Jacksonville, FL",
+      ytlink: "https://www.youtube.com/watch?v=JvjqEokuDYo",
+      videoID: "JvjqEokuDYo",
+    },
+    {
+      Title: "vannevannela gopakannelE",
+      Description:
+        "This song was written by Dr Uma Eyyunni and was set to music and orchestrated by Srinivasa Bharadwaj Kishore.  It was sung by Radhika Nori, Himabindu Janapati, Madhavi Mundrati, Subhadra Murthy, Sandhya Eswari, Sirisha Jakkala, Sushma Mukhirala.",
+      Date: 2014,
+      Location: "NATA in Atlanta, GA",
+      ytlink: "https://www.youtube.com/watch?v=eHIS-c2QNCA",
+      videoID: "eHIS-c2QNCA",
+    },
+  ],
+};
 
 function makeMV(mvData) {
-    let mvShell = document.createElement("div");
-    mvShell.setAttribute("class", "mv-shell");
+  let mvShell = document.createElement("div");
+  mvShell.setAttribute("class", "mv-shell");
 
-    let mvText = document.createElement("div");
-    mvText.setAttribute("class", "mv-text");
+  let mvText = document.createElement("div");
+  mvText.setAttribute("class", "mv-text");
 
-    let mvTitle = document.createElement("div");
-    mvTitle.setAttribute("class", "mv-title");
-    mvTitle.textContent = mvData.Title
+  let mvTitle = document.createElement("div");
+  mvTitle.setAttribute("class", "mv-title");
+  mvTitle.textContent = mvData.Title;
 
-    let mvDate = document.createElement("div");
-    mvDate.setAttribute("class", "mv-date");
-    mvDate.textContent = mvData.Date
+  let mvDate = document.createElement("div");
+  mvDate.setAttribute("class", "mv-date");
+  mvDate.textContent = mvData.Date;
 
-    let mvDesc = document.createElement("div");
-    mvDesc.setAttribute("class", "mv-desc");
-    mvDesc.textContent = mvData.Description
+  let mvDesc = document.createElement("div");
+  mvDesc.setAttribute("class", "mv-desc");
+  mvDesc.textContent = mvData.Description;
 
-    let mvContainer = document.createElement("div");
-    mvContainer.setAttribute("class", "mv-container");
+  let mvContainer = document.createElement("div");
+  mvContainer.setAttribute("class", "mv-container");
 
-    let mv = document.createElement("iframe");
-    mv.setAttribute("width", "100%");
-    mv.setAttribute("height", "240px");
-    mv.setAttribute("style", "border-radius: 20px; padding: 10px");
-    mv.setAttribute("src", "https://www.youtube-nocookie.com/embed/" + mvData.videoID);
-    mv.setAttribute("frameborder", "0");
-    mv.setAttribute("allow", "allow", "fullscreen; accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture");
+  let mv = document.createElement("iframe");
+  mv.setAttribute("width", "100%");
+  mv.setAttribute("height", "240px");
+  mv.setAttribute("style", "border-radius: 20px; padding: 10px");
+  mv.setAttribute(
+    "src",
+    "https://www.youtube-nocookie.com/embed/" + mvData.videoID
+  );
+  mv.setAttribute("frameborder", "0");
+  mv.setAttribute(
+    "allow",
+    "allow",
+    "fullscreen; accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+  );
 
-    mvContainer.appendChild(mv);
+  mvContainer.appendChild(mv);
 
-    mvText.appendChild(mvTitle);
-    mvText.appendChild(mvDate);
-    mvText.appendChild(mvDesc);
+  mvText.appendChild(mvTitle);
+  mvText.appendChild(mvDate);
+  mvText.appendChild(mvDesc);
 
-    mvShell.appendChild(mvText);
-    mvShell.appendChild(mvContainer);
+  mvShell.appendChild(mvText);
+  mvShell.appendChild(mvContainer);
 
-    return mvShell;
-
-
+  return mvShell;
 }
 
-
 function populateMV() {
-    let main = document.getElementById("mv-box");
-    mvData.data.forEach((mv) => {
-        main.appendChild(makeMV(mv));
-    });
+  let main = document.getElementById("mv-box");
+  mvData.data.forEach((mv) => {
+    main.appendChild(makeMV(mv));
+  });
 }
 
 populateMV();
-
